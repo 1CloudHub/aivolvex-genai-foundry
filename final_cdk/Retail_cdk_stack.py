99842f9a3e839b10c6b9507b7b9f1845a06718ca
abishekvvn0906 <abhishek.venu@1cloudhub.com>
2026-07-01 12:30:41 +0530

    Update Retail_cdk_stack.py

--- final_cdk/Retail_cdk_stack.py
+++ final_cdk/Retail_cdk_stack.py
@@ -818,7 +818,7 @@ class RetailCdkStack(Stack):
         db_instance = rds.DatabaseInstance(
             self, "MyPostgreSQLDB",
             engine=rds.DatabaseInstanceEngine.postgres(
-                version=rds.PostgresEngineVersion.VER_16_3
+                version=rds.PostgresEngineVersion.VER_17_5
             ),
             instance_type=ec2.InstanceType.of(
                 ec2.InstanceClass.T3,
