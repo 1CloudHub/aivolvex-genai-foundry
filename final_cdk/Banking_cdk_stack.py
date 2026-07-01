4c95578761a2f6d7a93455106aa2aef001932d44
abishekvvn0906 <abhishek.venu@1cloudhub.com>
2026-07-01 11:46:06 +0530

    Upgrade PostgreSQL version to 17.5
    testing banking cdk

--- final_cdk/Banking_cdk_stack.py
+++ final_cdk/Banking_cdk_stack.py
@@ -932,7 +932,7 @@ class BankingCdkStack(Stack):
         db_instance = rds.DatabaseInstance(
             self, "MyPostgreSQLDB",
             engine=rds.DatabaseInstanceEngine.postgres(
-                version=rds.PostgresEngineVersion.VER_17_4
+                version=rds.PostgresEngineVersion.VER_17_5
             ),
             instance_type=ec2.InstanceType.of(
                 ec2.InstanceClass.T3,
